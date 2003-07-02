3a8f24e1e675c4d8d5b6643dc02427ce7557ded1
wellwine <wellwine>
2003-07-02 15:17:49 +0000

    Add some phrases for English page

--- language/english/admin.php
+++ language/english/admin.php
@@ -1,9 +1,9 @@
 <?php
-// $Id: admin.php,v 1.2 2003/06/28 11:33:05 nao-pon Exp $
+// $Id: admin.php,v 1.3 2003/07/02 15:17:49 wellwine Exp $
 // FIXME: not good at English. :P
 
-define("_AM_WIKI_TITLE1", "PukiWiki Basic Settings");
-define("_AM_WIKI_TITLE2", "Chane Permit");
+define("_AM_WIKI_TITLE1", "Preferences");
+define("_AM_WIKI_TITLE2", "Change Permission");
 define("_AM_WIKI_SUBMIT", "Submit");
 define("_AM_WIKI_ENABLE", "enable");
 define("_AM_WIKI_DISABLE", "disable");
@@ -17,15 +17,22 @@ define("_AM_DBUPDATED", "The writing to a file was completed.");
 define("_AM_WIKI_ERROR01", "not writable");
 
 define("_AM_WIKI_DEFAULTPAGE", "Default Page");
-define("_AM_WIKI_MODIFIER", "Modifire");
-define("_AM_WIKI_MODIFIERLINK", "Modidire Link");
+define("_AM_WIKI_MODIFIER", "Modifier");
+define("_AM_WIKI_MODIFIERLINK", "Modifier's Homepage");
 define("_AM_WIKI_FUNCTION_FREEZE", "Enable Freeze");
-define("_AM_WIKI_ADMINPASS", "The administrator password for freeze release<br>（Please enter, only when you change a password.）");
-define("_AM_WIKI_CSS", "The override of a style sheet<br />（テーマによって見出し等が非常に見づらくなったり、<br />Wikiの色を変えたい時に有効です）");
+define("_AM_WIKI_ADMINPASS", "The password to unfreeze<br>（type a new password only when changing it.）");
+define("_AM_WIKI_CSS", "Styles of page that override default style<br />(This can be useful to change style of PukiWiki pages to fit them your theme)");
 
-define("_AM_WIKI_PERMIT_CHANGE", "A directory with the file which wants to change a permission<br>（We do not recommend you to use this function in addition to the time of modular deletion.<br />The thing wrote in only nobody and it became impossible is set to 0666.）");
-define("_AM_WIKI_ANONWRITABLE", "Edit of an anonymity user is permitted.");
-define("_AM_WIKI_HIDE_NAVI", "When a page is frozen, a upside navigation bar is hidden.(Only Webmaster is displayed.)");
+define("_AM_WIKI_PERMIT_CHANGE", "A directory with the file which wants to change a permission<br>(Recommended to use this for uninstalling this module only<br />Basically chmod to 0666)");
+define("_AM_WIKI_ANONWRITABLE", "Who is permitted to edit pages");
+define("_AM_WIKI_HIDE_NAVI", "Hide the navigation menu at the top of pages hen frozen (Always shown if Webmasters logged in)");
+define("_AM_WIKI_MAIL_SW", "Notify by email when edited by");
+define("_AM_WIKI_ALL", "All users");
+define("_AM_WIKI_REGIST", "All registered users");
+define("_AM_WIKI_ADMIN", "Webmasters only");
+define("_AM_WIKI_MAIL_ALL", "All users");
+define("_AM_WIKI_MAIL_NOADMIN", "All users except Webmasters");
+define("_AM_WIKI_MAIL_NONE", "Nobody");
 
 ?>
 
