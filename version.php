5eb7580e4c517500f2ae0cbbc96da37a43feb7d4
nao-pon <hypweb@gmail.com>
2005-04-17 12:59:04 +0000

    Ver 1.2.5

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: version.php,v 1.32 2005/03/29 23:55:49 nao-pon Exp $
+// $Id: version.php,v 1.33 2005/04/17 12:59:04 nao-pon Exp $
 /////////////////////////////////////////////////
-define("_XOOPS_WIKI_VERSION", "1.2.4.1");
+define("_XOOPS_WIKI_VERSION", "1.2.5");
 ?>
