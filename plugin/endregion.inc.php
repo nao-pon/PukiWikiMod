ea2ea5da957d986e92dd12c5d34b2eb7ba7ab7fa
nao-pon <hypweb@gmail.com>
2006-03-07 02:12:28 +0000

    added region plugin(region.inc.php, endregion.inc.php)

---
+++ plugin/endregion.inc.php
@@ -0,0 +1,14 @@
+<?php
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: endregion.inc.php,v 1.1 2006/03/07 02:12:28 nao-pon Exp $
+//
+
+function plugin_endregion_convert()
+{
+	return <<<EOD
+</td></tr></table>
+EOD;
+}
+?>
