9ab364c32ce5249d05131dd8dfbf98e3af46408e
nao-pon <hypweb@gmail.com>
2006-05-12 00:09:30 +0000

    added Id: retry :-P

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: version.php,v 1.52 2006/04/23 01:57:59 nao-pon Exp $
+// $Id: version.php,v 1.53 2006/05/12 00:09:30 nao-pon Exp $
 /////////////////////////////////////////////////
-if (! defined("_XOOPS_WIKI_VERSION"))define("_XOOPS_WIKI_VERSION", "1.6.1");
+if (! defined("_XOOPS_WIKI_VERSION"))define("_XOOPS_WIKI_VERSION", "1.6.2");
 ?>
