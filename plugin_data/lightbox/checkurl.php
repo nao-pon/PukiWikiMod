e3094824201fed2fdee2ed7b25aee51b1ef0b3c1
nao-pon <hypweb@gmail.com>
2006-06-28 00:31:35 +0000

    added Content-Type check.

--- plugin_data/lightbox/checkurl.php
+++ plugin_data/lightbox/checkurl.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(0);
-$url = (empty($_GET['q']))? "" : $_GET['q'];
+$url = (empty($_GET['q']))? "" : str_replace("\0","",$_GET['q']);
 
+if (!preg_match("/^http/i",$url)) exit("0");
 if (!$url) exit("0");
 
 include ("../../include/hyp_common/hyp_common_func.php");
@@ -13,6 +14,15 @@ $d->method = 'HEAD';
 $d->ua = 'Mozilla/4.0';
 $d->read_timeout = 3;
 $d->get();
+
+if (preg_match("/^Content-Type:(.+)$/im",$d->header,$match))
+{
+	$type = trim($match[1]);
+	if (!preg_match("/^image/i",$type))
+	{
+		$d->rc = 404;
+	}
+}
 
 exit("$d->rc");
 
