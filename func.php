fdb1816f80b68ca2257c53bb416f62e83c599ffc
nao-pon <hypweb@gmail.com>
2007-12-06 04:21:08 +0000

    Adjustment of auto link pattern generation method.

--- func.php
+++ func.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: func.php,v 1.80 2006/08/04 12:24:20 nao-pon Exp $
+// $Id: func.php,v 1.81 2007/12/06 04:21:08 nao-pon Exp $
 /////////////////////////////////////////////////
 if (!defined("PLUGIN_INCLUDE_MAX")) define("PLUGIN_INCLUDE_MAX",4);
 
@@ -1010,10 +1010,7 @@ function get_autolink_pattern(& $pages)
 	}
 	else
 	{
-		$auto_pages = array_unique($auto_pages);
-		sort($auto_pages, SORT_STRING);
-
-		$result = get_autolink_pattern_sub($auto_pages, 0, count($auto_pages), 0);
+		$result = get_matcher_regex_safe($auto_pages);
 	}
 	
 	return array($result, '(?!)', $forceignorepages);
@@ -1089,6 +1086,97 @@ function get_autolink_pattern_sub(& $pages, $start, $end, $pos)
 	}
 	$lev --;
 	return $result;
+}
+
+function get_matcher_regex_safe ($pages, $spliter = "\t", $array_fix = true, $nest = 0) {
+	
+	if ($array_fix) {
+		$pages = array_map('trim', $pages);
+		$pages = array_unique($pages);
+		foreach(array_keys($pages, '') as $key) {
+			unset($pages[$key]);
+		}
+		sort($pages, SORT_STRING);
+	}
+	
+	++$nest;
+	$reg = get_matcher_regex_safe_sub($pages);
+	$regs = preg_split("/(\d+)\x08/", $reg, -1, PREG_SPLIT_DELIM_CAPTURE);
+	$pats = array();
+	$index = 0;
+	reset($regs);
+	while (list($key, $pat) = each($regs)) {
+		list($key, $val) = each($regs);
+		if (!$val) $val = count($pages);
+		if (@ preg_match('/' . $pat. '/', '') === false) {
+			if ($nest <= 10) {
+				$count = $val - $index;
+				$split = floor(($val - $index) / 2);
+				$pages1 = array_slice($pages, $index, $split);
+				$pages2 = array_slice($pages, $split, $count - $split);
+				$pats[] = get_matcher_regex_safe($pages1, $spliter, false, $nest);
+				$pats[] = get_matcher_regex_safe($pages2, $spliter, false, $nest);
+				$index = $val;
+			}
+		} else {
+			$pats[] = $pat;
+		}
+	}
+	return join($spliter, $pats);
+}
+
+function get_matcher_regex_safe_sub (& $array, $offset = 0, $sentry = NULL, $pos = 0, $nest = 0)
+{
+	++$nest;
+	$limit = 1024 * 32 - 10;
+	
+	if (empty($array)) return '(?!)'; // Zero
+	if ($sentry === NULL) $sentry = count($array);
+	
+	// Too short. Skip this
+	$skip = ($pos >= mb_strlen($array[$offset]));
+	if ($skip) ++$offset;
+
+	// Generate regex for each value
+	$regex = '';
+	$index = $offset;
+	$multi = FALSE;
+	$reglen = 0;
+	while ($index < $sentry) {
+		if ($index != $offset) {
+			$multi = TRUE;
+			if ($nest === 1 && strlen($regex) - $reglen > $limit) {
+				$reglen = strlen($regex);
+				$regex .= ')'.($index)."\x08(?:";
+			} else {
+				$regex .= '|'; // OR
+			}
+		}
+
+		// Get one character from left side of the value
+		$char = mb_substr($array[$index], $pos, 1);
+
+		// How many continuous keys have the same letter
+		// at the same position?
+		for ($i = $index; $i < $sentry; $i++)
+			if (mb_substr($array[$i], $pos, 1) != $char) break;
+		
+		if ($index < ($i - 1)) {
+			// Some more keys found
+			// Recurse
+			$regex .= str_replace(' ', '\\ ', preg_quote($char, '/')) .
+			get_matcher_regex_safe_sub($array, $index, $i, $pos + 1, $nest);
+		} else {
+			// Not found
+			$regex .= str_replace(' ', '\\ ',
+			preg_quote(mb_substr($array[$index], $pos), '/'));
+		}
+		$index = $i;
+	}
+	
+	if ($skip || $multi) $regex = '(?:' . $regex . ')';
+	if ($skip) $regex .= '?'; // Match for $pages[$offset - 1]
+	return $regex;
 }
 
 // ページ名を指定階層数切り詰める
