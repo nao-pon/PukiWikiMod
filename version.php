a34f040599e42951f2f9eb549f4f607f8ead6cdd
nao-pon <hypweb@gmail.com>
2008-06-26 00:50:35 +0000

    Version 1.6.6.2

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: version.php,v 1.60 2008/02/25 03:10:04 nao-pon Exp $
+// $Id: version.php,v 1.61 2008/06/26 00:50:35 nao-pon Exp $
 /////////////////////////////////////////////////
-if (! defined("_XOOPS_WIKI_VERSION"))define("_XOOPS_WIKI_VERSION", "1.6.6.1");
+if (! defined("_XOOPS_WIKI_VERSION"))define("_XOOPS_WIKI_VERSION", "1.6.6.2");
 ?>
